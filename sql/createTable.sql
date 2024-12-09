17174ab99b27c869ae15f50d1872b6c5f0e50316
coconutkim <achaeyeon18@gmail.com>
2024-12-09 12:17:55 +0900

    update ppt and modify query

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -5,10 +5,15 @@ create database movie;
 use movie;
 
 create table posters (
-	image longblob not null,
+    id int auto-increment primary key,
+	image longblob not null, 
     title varchar(255) not null,
+    relYear int not null,
+    rating varchar(20) not null,
+    runTime float not null,
     genre varchar(255) not null,
     director varchar(255) not null,
+    story varchar(2000) not null,
     actor varchar(255) not null,
     prod varchar(255) not null,
     nation varchar(255) not null,
@@ -17,11 +22,11 @@ create table posters (
 
 select * from posters;
 
-alter table posters
-add column story varchar(2000) not null after actor,
-add column relYear int not null after title,
-add column rating varchar(20) not null after relYear,
-add column runTime float not null after rating;
+-- alter table posters
+-- add column story varchar(2000) not null after actor,
+-- add column relYear int not null after title,
+-- add column rating varchar(20) not null after relYear,
+-- add column runTime float not null after rating;
 
 
 -- 기존에 존재하는 데이터베이스를 버린다  
