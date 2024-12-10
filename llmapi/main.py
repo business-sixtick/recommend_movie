aa499eba7d329d5c51c8c41c2c9bdd8592785ba8
sixtick_ubuntu <business4dyd@gmail.com>
2024-12-10 12:41:25 +0900

    llm 서버에 라마 연동

--- llmapi/main.py
+++ llmapi/main.py
@@ -1,5 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI    # FastAPI 는 Starlette 를 직접 상속하는 클래스임.
 from fastapi.staticfiles import StaticFiles
+
+from langchain.chains import LLMChain
+from langchain import PromptTemplate
+from langchain.llms.ollama import Ollama # ollama LLM모델
+
+llm = Ollama(base_url='http://127.0.0.1:11434', model='kor8b')
+prompt = PromptTemplate(
+    input_variables = ['description'],
+    template=(
+        "다음 설명에 어울리는 영화 제목을 콤마로 구분된 문자열로 반환해줘:\n"
+        "설명: {description}\n"
+        "응답 형식: 영화 제목1, 영화 제목2, ..."
+    )
+)
+chain = LLMChain(llm = llm , prompt = prompt)
+chain.run('대한민국')
 
 # FastAPI 애플리케이션 인스턴스 생성
 app = FastAPI()
@@ -9,10 +25,28 @@ app = FastAPI()
 # async def read_root():
 #     return {"message": "Hello, World!"}
 
-# "static" 디렉토리를 정적 파일 제공 경로로 설정
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 # 경로 "/items/{item_id}"에 대한 GET 요청 처리
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/text/{text}")
+async def read_text(text: str, q: str = None):
+    return {"text": text, "q": q}
+
+
+@app.get("/llm/{text}")
+async def read_llm(text: str, q: str = None):
+    """
+    ex)
+    /llm/질의내용
+    """
+    answer = chain.run(text)
+    return {"llm": text, "q": q, "answer" : answer}
+
+
+# "static" 디렉토리를 정적 파일 제공 경로로 설정
+app.mount("/", StaticFiles(directory="static", html=True), name="static")   # / 를 라우팅 할때 다른것들과 충돌 함. 맨 아래로 이동했음. 또는 경로를 /name 명확히 할 수 도 있음. 
+
